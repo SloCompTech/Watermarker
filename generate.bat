13793a2a598c5f1eba2cce02897512d8f9d38bd2
Martin Dagarin <SloCompTech@users.noreply.github.com>
2018-06-09 19:49:43 +0200

    Added binary file generate script

---
+++ generate.bat
@@ -0,0 +1,10 @@
+pyinstaller watermarker.py ^
+--onefile ^
+--name watermarker ^
+--add-data fonts;fonts ^
+--add-data LICENSE.md;LICENSE.md ^
+--add-data LICENSE.pillow.md;LICENSE.pillow.md ^
+--add-data README.md;README.md ^
+--log-level DEBUG
+
+:: TODO add icon with --icon <ico file>
